8df7caa0291e8d9710bca023269809daf1e5e0ff
nathalia2021 <nathalia_porra23151@elpoli.edu.co>
2021-09-15 14:16:26 -0500

    taller de eureka service

---
+++ src/test/java/taller/com/co/register/RegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package taller.com.co.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
